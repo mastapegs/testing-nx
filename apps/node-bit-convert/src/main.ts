956b92ff80b72048c52f25aaef37d3a18f12195f
Matthew Pagan <mastapegs01@gmail.com>
2021-10-01 22:34:50 -0400

    feat: create node app to test lib

---
+++ apps/node-bit-convert/src/main.ts
@@ -0,0 +1,6 @@
+import {convertAsciiStringToBinaryString} from '@testing-nx/convert-to-bits'
+
+const testString = 'Hello, World!'
+
+console.log(testString)
+console.log(convertAsciiStringToBinaryString(testString))

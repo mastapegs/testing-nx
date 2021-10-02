2977a67995817142b72c56957fdfc286c01d05dc
Matthew Pagan <mastapegs01@gmail.com>
2021-10-01 21:41:09 -0400

    feat: increased testing for bit solution

--- libs/convert-to-bits/src/lib/convert-to-bits.spec.ts
+++ libs/convert-to-bits/src/lib/convert-to-bits.spec.ts
@@ -1,10 +1,16 @@
-import { convertToBits } from './convert-to-bits';
+import { convertStringToBits, convertAsciiToOnBits } from './convert-to-bits';
 
 describe('convertToBits', () => {
+  it('should correctly convert an ascii code to on bit representation', () => {
+    expect(convertAsciiToOnBits(65)).toEqual(2)
+    expect(convertAsciiToOnBits(66)).toEqual(2)
+    expect(convertAsciiToOnBits(67)).toEqual(3)
+  })
+
   it('should correctly convert a string to on bit representation', () => {
-    expect(convertToBits("ABC")).toEqual(7);
-    expect(convertToBits("ABB")).toEqual(6);
-    expect(convertToBits("ABCD")).toEqual(9);
-    expect(convertToBits("ABAB")).toEqual(8);
+    expect(convertStringToBits("ABC")).toEqual(7);
+    expect(convertStringToBits("ABB")).toEqual(6);
+    expect(convertStringToBits("ABCD")).toEqual(9);
+    expect(convertStringToBits("ABAB")).toEqual(8);
   });
 });
--- libs/convert-to-bits/src/lib/convert-to-bits.ts
+++ libs/convert-to-bits/src/lib/convert-to-bits.ts
@@ -1,22 +1,22 @@
 export function convertAsciiToOnBits(ascii: number): number {
-  let onCount = 0
-  let numberInProgress = ascii
+  let onCount = 0;
+  let numberInProgress = ascii;
   for (let i = 7; i >= 0; i--) {
     if (numberInProgress / Math.pow(2, i) >= 1) {
-      onCount++
-      numberInProgress %= Math.pow(2, i)
+      onCount++;
+      numberInProgress %= Math.pow(2, i);
     }
   }
-  return onCount
+  return onCount;
 }
 
-export function convertToBits(input: string): number {
-  const asciiArray: number[] = []
+export function convertStringToBits(input: string): number {
+  const asciiArray: number[] = [];
   for (let i = 0; i < input.length; i++) {
-    asciiArray.push(input.charCodeAt(i))
+    asciiArray.push(input.charCodeAt(i));
   }
 
-  const individualOnBits = asciiArray.map(ascii => convertAsciiToOnBits(ascii))
-
-  return individualOnBits.reduce((accu, curr) => accu + curr, 0)
+  return asciiArray
+    .map((ascii) => convertAsciiToOnBits(ascii))
+    .reduce((accu, curr) => accu + curr, 0);
 }
